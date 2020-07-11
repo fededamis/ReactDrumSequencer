86e932972c2b0445cb36dfa31952821b70279dc0
fededamis <fede_damis@hotmail.com>
2020-07-11 01:09:48 -0300

    Se agrega contenido del Footer, se agregan estilos de PlayButton.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return (
-            <div>
-               <p>FOOTER</p>
+            <div className='Footer'>
+               <p>Desarrollado por <a href='https://fededamis.github.io/CVDigital/' target="_blank">
+               Federico Damis</a>. Para ver el código fuente en Github haga 
+               click <a href='https://github.com/fededamis/ReactDrumSequencer' target="_blank">aquí</a>.</p> 
             </div>        
         );
     }    
--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { connect } from "react-redux";
+import cx from 'classnames'
 
 class PlayButton extends React.Component {
     render() {
         return (
-            <button className="PlayButton"
+            <button className={cx('PlayButton',{
+                started: this.props.playing,
+                paused: !this.props.playing
+            })}
             onClick={() => {this.props.play()}}>Play</button>
         );
     }    
 }
+ 
+const mapStateToProps = state => ({
+    playing: state.playing    
+}); 
 
 const mapDispatchToProps = dispatch => ({
     play: () => dispatch({ type: 'PLAY' }),
 }); 
 
-export default connect(null, mapDispatchToProps)(PlayButton);   
+export default connect(mapStateToProps, mapDispatchToProps)(PlayButton);   
